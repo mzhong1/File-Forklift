60130e45b4a436ac9d8dac0707e63d9001159dd2
mzhong215 <Michelle_Zhong@comcast.com>
2018-10-01 09:03:27 -0400

    Added comments to main of algorithm

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,16 @@ extern crate zmq;
 use std::fs::File;
 use std::io::BufReader;
 use std::io::BufRead;
-use std::time::Duration;
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
+
+//In a worker (Dealer socket):
+//Calculate liveness (how many missed heartbeats before assuming death)
+// wait in zmq_poll loop one sec at a time 
+//if message from other worker?  router?  reset liveness
+//if no message count down
+//if liveness reaches zero, consider the node dead.  
+
+
 fn main()
 {
     //later, when we need to get node names + ip addresses
@@ -12,10 +21,37 @@ fn main()
 
     let num_nodes = 3; //Later we'll make it so we can set the number of nodes based on the 
     //file or whatever input
-    let heartbeart_duration = Duration::new(5,0);
+    //set liveness
+    let liveness = 5; //number of times we can miss a tick
+    //set heartbeat interval
+    let interval = 1000; //msecs
+    //set heartbeat_at
+    let start = SystemTime::now();
+    let since_epoch = start.duration_since(UNIX_EPOCH).expect("Time went backwards whoops");
+    let heartbeat_at = (since_epoch.as_secs() * 1000 + since_epoch.subsec_nanos() as u64 / 1_000_000) + interval;
 
+    //Make two sockets, one Dealer, one Router
+    //The Dealer will handle heartbeat messages sent to it
+    //The Router will send OUT heartbeats
+    //The Dealer will BIND to one network 
+    //The Router will BIND to another
     let context = zmq::Context::new();
-    let broker = context.socket(zmq::ROUTER).unwrap();
+    let router = context.socket(zmq::ROUTER).unwrap();
 
+
+    //Build a list of DEALER sockets that the ROUTER sends to from node_names
+    //the DEALER sockets connect to the ip addresses of their machines
+    //the ROUTER will send out heartbeat messages to these machines every second 
+    //using a loop over the DEALER sockets
+
+
+
+    //Poll THIS machines DEALER and ROUTER
+    //Pollin using timeout of 
+    //two if loops, handle POLLIN
+    //if DEALER POLLIN => recieved heartbeat message from some socket (ip address of the heartbeat sender)
+    //handle heartbeat by: 
+    //unpacking message to find out sender
+    //update the liveness
 
 }
