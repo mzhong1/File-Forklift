958067b5d17bece9b2448088a0a457bc01585d63
mzhong215 <Michelle_Zhong@comcast.com>
2018-10-04 17:31:09 -0400

    Added some command line argument handling, filled out flatbuffer message mod, moved some main function behavior into functions

--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,39 @@ extern crate local_ip;
 extern crate zmq;
 #[macro_use]
 extern crate log;
+#[macro_use]
 extern crate clap;
 extern crate dirs;
-extern crate simplelog;
 extern crate nanomsg;
+extern crate simplelog;
 
-use nanomsg::{Socket, Protocol, PollFd, PollRequest, PollInOut};
 use clap::{App, Arg};
+use nanomsg::{PollFd, PollInOut, PollRequest, Protocol, Socket};
 use std::collections::HashMap;
 use std::fs::{File, OpenOptions};
 use std::io::BufRead;
 use std::io::BufReader;
 use std::path::Path;
 use std::time::{SystemTime, UNIX_EPOCH};
+mod mess;
 mod node;
 use node::Node;
-use simplelog::{Config, WriteLogger};
+use simplelog::{CombinedLogger, Config, SharedLogger, TermLogger, WriteLogger};
 
 /*
-current_time_in_millis: SystemTime -> u64
-REQUIRES: start is the current System Time
-ENSURES: returns the time since the UNIX_EPOCH in milliseconds
+    Heartbeat protocol
+    In a worker (Dealer socket):
+    Calculate liveness (how many missed heartbeats before assuming death)
+    wait in zmq_poll loop one sec at a time
+    if message from other worker?  router?  reset liveness
+    if no message count down
+    if liveness reaches zero, consider the node dead.
+*/
+
+/*
+    current_time_in_millis: SystemTime -> u64
+    REQUIRES: start is the current System Time
+    ENSURES: returns the time since the UNIX_EPOCH in milliseconds
 */
 fn current_time_in_millis(start: SystemTime) -> u64 {
     let since_epoch = start
@@ -33,43 +45,110 @@ fn current_time_in_millis(start: SystemTime) -> u64 {
 }
 
 /*
-get_port: &str * Vec<String> -> String
-REQUIRES: s an ip address, nodes is not empty
-ENSURES: returns the port associated with the input ip, otherwise
-return ""
+    get_port: &str * Vec<String> -> String
+    REQUIRES: s an ip address, nodes is not empty
+    ENSURES: returns the port associated with the input ip, otherwise
+    return ""
 */
-fn get_port(s : &str, nodes: Vec<String>) -> String
-{
+fn get_port(s: String, nodes: Vec<String>) -> String {
     let mut port = "".to_string();
-    for n in nodes{
-        if n.contains(s)
-        {
-            let splitip = n.split(s);
+    for n in nodes {
+        if n.contains(&s) {
+            let splitip = n.split(&s);
             let vec = splitip.collect::<Vec<&str>>();
-            port = vec[vec.len() -1].to_string();
+            port = vec[vec.len() - 1].to_string();
         }
     }
     port
 }
-/*Heartbeat protocol
-{
-    In a worker (Dealer socket):
-    Calculate liveness (how many missed heartbeats before assuming death)
-    wait in zmq_poll loop one sec at a time
-    if message from other worker?  router?  reset liveness
-    if no message count down
-    if liveness reaches zero, consider the node dead.
+
+fn make_nodelist(filename: &str) -> Vec<String> {
+    BufReader::new(File::open(filename).expect("Cannot open file"))
+        .lines()
+        .collect::<Result<_, _>>()
+        .expect("cannot read words")
 }
+
+fn make_nodemap(
+    node_names: &Vec<String>,
+    ip_address: String,
+    liveness: i64,
+) -> HashMap<String, Node> {
+    //create mutable hashmapof nodes
+    let mut nodes = HashMap::new();
+    //fill in vectors with default values
+    for node_ip in node_names {
+        if node_ip != &ip_address {
+            debug!("node ip addresses and port: {}", node_ip);
+            let mut temp_node = Node::new(node_ip);
+            temp_node.liveness = liveness;
+            debug!("Node successfully created : {:?}", &temp_node);
+            nodes.insert(node_ip.to_string(), temp_node);
+        }
+    }
+    nodes
+}
+
+fn make_node(ip_address: String, node_names: Vec<String>) -> Socket {
+    let mut router = match Socket::new(Protocol::Bus) {
+        Ok(socket) => socket,
+        Err(err) => panic!("{}", err),
+    };
+    let current_port = get_port(ip_address, node_names);
+    assert!(
+        router
+            .bind(&format!("tcp://*{}", current_port))
+            .is_ok()
+    );
+    router
+}
+
+fn connect_node(node_ip: &String, router: Socket) {
+    let mut tcp: String = "tcp://".to_owned();
+    tcp.push_str(node_ip);
+    assert!(router.connect(&tcp).is_ok());
+}
+
+fn init_logs(f: &Path, level: simplelog::LevelFilter) {
+    if !f.exists() {
+        File::create(f).expect("Creating log file failed");
+    }
+    let mut loggers: Vec<Box<SharedLogger>> = vec![];
+    if let Some(term_logger) = TermLogger::new(level, Config::default()) {
+        loggers.push(term_logger);
+    }
+    loggers.push(WriteLogger::new(level, Config::default(), File::open(f)));
+    let _ = CombinedLogger::init(loggers);
+    info!("Starting up");
+}
+
+/*
+    main takes in two flags: 
+    j: computer is a new node, not a part of the original list
+    d: create debug logs
+    When the 'j' flag is raised, the program takes in the arguments ip_addr:port, otherip_addr:port
+    Without the 'j' flag, the program takes in a file argument of ip_addr:port 
+    addresses of all nodes in the graph
 */
 fn main() {
     let path = dirs::home_dir().unwrap().join("debuglog");
     let matches = App::new("Heartbeat Logs")
-        .author("Michelle")
-        .about("Debug logs for the heartbeat program")
+        .author(crate_authors!())
+        .about("NFS and Samba filesystem migration program")
+        .version(crate_version!())
+        .arg(
+            Arg::with_name("namelist")
+            .help("Name of the file to pull the node list from")
+            .long("namelist")
+            .short("n")
+            .takes_value(true)
+            .required(true)
+            .conflicts_with("join")
+        )
         .arg(
             Arg::with_name("logfile")
-                .default_value(path.to_str().unwrap())
-                .help("File to log to")
+                .default_value(&path.to_string_lossy())
+                .help("Logs debug statements to file debuglog")
                 .long("logfile")
                 .short("l")
                 .takes_value(true)
@@ -79,39 +158,37 @@ fn main() {
                 .short("v")
                 .multiple(true)
                 .help("Sets the level of verbosity"),
+        ).arg(
+            Arg::from_usage("-j, --join <YOURIP:PORT> 
+            'The ip address and port of your node in the form of IP:PORT' 
+            <NODEIP:PORT> 'The ip address and port of a live node to connect 
+            to in the form IP:PORT'")
+            .required(false)
+            .help("Joins a node not in the graph to the graph"),
         ).get_matches();
     let level = match matches.occurrences_of("v") {
         0 => simplelog::LevelFilter::Info,
         1 => simplelog::LevelFilter::Debug,
         _ => simplelog::LevelFilter::Trace,
     };
-    let logfile = matches.value_of("logfile").unwrap();
-    if !Path::new(logfile).exists() {
-        File::create(logfile).expect("Creating log file failed");
-    }
-    WriteLogger::init(
-        level,
-        Config::default(),
-        OpenOptions::new().append(true).open(logfile).unwrap(),
-    ).unwrap();
+    let logfile = Path::new(matches.value_of("logfile").unwrap());
+    init_logs(&logfile, level);
 
-    println!("The path of the debuglog is {}", path.to_str().unwrap());
+    let joined = match matches.values_of("join") {
+        None => vec![],
+        Some(T) => T.collect()
+    };
 
-    //later, when we need to get node ip addresses + port
-    let filename = "nodes.txt";
-    let node_names: Vec<_> = BufReader::new(File::open(filename).expect("Cannot open file"))
-        .lines()
-        .collect::<Result<_, _>>()
-        .expect("cannot read words");
+    let filename = match matches.value_of("namelist")
+    {
+        None => "",
+        Some(T) => T
+    }; 
+    
 
-    //local ip address
-    let ip_address = local_ip::get().unwrap().to_string();
-    debug!("current ip address: {}", ip_address);
-
-    //set liveness, number of times we can miss a tick
-    let liveness = 5;
-    //set heartbeat interval in msecs
-    let interval = 1000;
+    //Variables that don't depend on command line args
+    let liveness = 5; //The amount of times we can tick down before assuming death
+    let interval = 1000;     //set heartbeat interval in msecs
     let start = SystemTime::now();
     let mut heartbeat_at = current_time_in_millis(start) + interval;
     debug!(
@@ -119,61 +196,58 @@ fn main() {
         heartbeat_at
     );
 
-    //create mutable hashmapof nodes
+    let mut ip_address = local_ip::get().unwrap().to_string();
     let mut nodes = HashMap::new();
-    //fill in vectors with default values
-    for node_ip in &node_names {
-        if node_ip != &ip_address {
-            debug!("node ip addresses and port: {}", node_ip);
-            let mut temp_node = Node::new(node_ip);
-            temp_node.liveness = liveness;
-            debug!("Node successfully created : {:?}", &temp_node);
-            nodes.insert(node_ip.to_string(), temp_node);
-        }
+    let mut node_names: Vec<String> = vec![]; 
+    if joined.len() > 0
+    //Do setup here
+    {
+        let tempvec = joined[0].split(":").collect::<Vec<&str>>();
+        ip_address = tempvec[0].to_string();
+        node_names.push(joined[1].to_string());
+        node_names.push(joined[0].to_string());
     }
-
-    //Make the node
-    let mut router = match Socket::new(Protocol::Bus){
-        Ok(socket) => socket,
-        Err(err) => panic!("{}", err)
-    };
-
-    let current_port = get_port(&ip_address, node_names);
-
-    assert!(router.bind(&format!("tcp://*{}", &current_port[..])).is_ok());
+    else
+    {
+        node_names = make_nodelist(filename);
+        nodes = make_nodemap(&node_names, ip_address, liveness); //create mutable hashmapof nodes
+        
+    }
+    debug!("current ip address, port: {}", ip_address);
+    let mut router = make_node(ip_address, node_names); //Make the node
     debug!("router created");
+
     //sleep for a bit to let other nodes start up
     std::thread::sleep(std::time::Duration::from_millis(10));
 
     //connect to addresses
     for node_ip in &node_names {
         if node_ip != &ip_address {
-            let mut tcp: String = "tcp://".to_owned();
-            tcp.push_str(node_ip);
-            assert!(router.connect(&tcp).is_ok());
+            connect_node(node_ip, router);
         }
     }
     debug!("Connection to nodes initiated");
 
-
-    //Poll THIS machine's DEALER
-    //Pollin using timeout of heartBeat interval
-    //two if loops, 1st handle POLLIN
-    //if DEALER POLLIN => recieved heartbeat message from some socket (ip address of the heartbeat sender)
-    //handle heartbeat by:
-    //unpacking message to find out sender
-    //update the liveness of the sender
-    //update array of bools associated with nodes, set had_heartbeat[i] to true (node had heartbeat)
-    //create pollitem for THIS machine's DEALER (should be mutable)
+    /*
+        Poll THIS machine's DEALER
+        Pollin using timeout of heartBeat interval
+        two if loops, 1st handle POLLIN
+        if DEALER POLLIN => recieved heartbeat message from some socket (ip address of the heartbeat sender)
+        handle heartbeat by:
+        unpacking message to find out sender
+        update the liveness of the sender
+        update array of bools associated with nodes, set had_heartbeat[i] to true (node had heartbeat)
+        create pollitem for THIS machine's DEALER (should be mutable)
+    */
     loop {
         debug!("Looped");
         println!("Looped");
 
         std::thread::sleep(std::time::Duration::from_millis(10));
         let mut msg = zmq::Message::new().unwrap();
-        let mut items : Vec<PollFd>  = vec![router.new_pollfd(PollInOut::In)];
+        let mut items: Vec<PollFd> = vec![router.new_pollfd(PollInOut::In)];
         let mut request = PollRequest::new(&mut items);
-        let result  = Socket::poll(&mut request, interval as isize);
+        let result = Socket::poll(&mut request, interval as isize);
         debug!("Poll can read: {:?}", request.get_fds()[0].can_read());
         println!("Poll can read: {:?}", request.get_fds()[0].can_read());
 
@@ -186,14 +260,12 @@ fn main() {
         }
         let start = SystemTime::now();
         /* 
-        {
             if current SystemTime (time since epoch in msec) > heartbeat_at
             send out heartbeats to list of other nodes (vector of DEALER sockets)
             loop through hashmap of bools (has_heartbeat), if false (did not recieve heartbeat within 1 sec)
             tick down liveness of associated node
             after ticking down, reset has_heartbeat values to false
             if liveness becomes 0 or less than 0, assume node is dead (handle it however necessary)
-        }
         */
         debug!("current time in millis {}", current_time_in_millis(start));
         debug!("heartbeat_at {}", heartbeat_at);
@@ -203,25 +275,19 @@ fn main() {
         update_nodes(
             start,
             &mut heartbeat_at,
-            &router,
+            router,
             &ip_address,
             &node_names,
             interval,
             &mut nodes,
         );
-        
     }
 }
-
-fn process_mess(data: &[u8]) -> Result<(), String>
-{
-
-} 
 
 fn update_nodes(
     start: SystemTime,
     heartbeat_at: &mut u64,
-    router: &zmq::Socket,
+    router: Socket,
     ip_address: &String,
     node_names: &Vec<String>,
     interval: u64,
--- src/mess.rs
+++ src/mess.rs
@@ -1,43 +1,41 @@
+extern crate api;
 extern crate flatbuffers;
 
-use api::service_generated::*;
+use self::api::service_generated::*;
 
-pub fn create_message(
-    mType: uint8,
-    message: Vec<String> 
-) -> &[u8]
-{
+pub fn create_message(m_type: MessageType, message: Vec<String>) -> Vec<u8> {
     let mut builder = flatbuffers::FlatBufferBuilder::new();
-    let mut nodes = builder.create_vector_of_strings(message);
-    let type = match mType {
-        0 => api::service_generated::MessageType::OHAI,
-        1 => api::service_generated::MessageType::GETLIST,
-        2 => api::service_generated::MessageType::NODELIST,
-        3 => api::service_generated::MessageType::HEARTBEAT,
-        _ => panic!("Invalid message type!")
-    }
+    let v: Vec<&str> = message.iter().map(|x| &**x).collect();
+    let mut nodes = builder.create_vector_of_strings(&v);
+    /*
+    let messageT = match mType {
+        0 => MessageType::OHAI,
+        1 => MessageType::GETLIST,
+        2 => MessageType::NODELIST,
+        3 => MessageType::HEARTBEAT,
+        // Might want to return Err here instead of panic
+        _ => panic!("Invalid message type!"),
+    };
+    */
     let create = Message::create(
         &mut builder,
-        &CreateRequestArgs {
-            mtype: type;
-            members: nodes;
+        &MessageArgs {
+            mtype: m_type,
+            members: Some(nodes),
         },
     );
     builder.finish_minimal(create);
-    builder.finished_data()
+    builder.finished_data().to_vec()
 }
 
-pub fn read_message(
-    buf: &[u8]
-) ->
-{
+pub fn read_message(buf: &[u8]) -> Option<Vec<String>> {
     let mess = get_root_as_message(buf);
     mess.members();
+
+    None
 }
 
-pub fn get_message_type(
-    buf: &[u8]
-){
+pub fn get_message_type(buf: &[u8]) {
     let mess = get_root_as_message(buf);
     mess.mtype();
 }
