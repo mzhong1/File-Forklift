28ac258380c5430e89b3eddee4d339c7cfb19451
mzhong215 <Michelle_Zhong@comcast.com>
2018-12-06 15:43:58 -0500

    copy_entry and checksum_copy now work for NFS

--- src/filesystem_entry.rs
+++ src/filesystem_entry.rs
@@ -1,6 +1,10 @@
+extern crate digest;
+extern crate meowhash;
 extern crate nix;
 extern crate smbc;
 
+use self::digest::Digest;
+use self::meowhash::*;
 use self::nix::fcntl::OFlag;
 use self::nix::sys::stat::{Mode, SFlag};
 use error::{ForkliftError, ForkliftResult};
@@ -8,7 +12,7 @@ use filesystem::*;
 use smbc::*;
 use std::path::{Path, PathBuf};
 
-const BUFF_SIZE: u64 = 1024 * 1024;
+const BUFF_SIZE: u64 = 1024 * 1000;
 
 #[derive(Clone)]
 pub struct Entry {
@@ -55,6 +59,7 @@ impl Entry {
 pub enum SyncOutcome {
     UpToDate,
     FileCopied,
+    FileUpdated,
     SymlinkUpdated,
     SymlinkCreated,
 }
@@ -222,6 +227,26 @@ pub fn has_different_permissions(
     }
 }
 
+fn make_target(size: i64, readmax: u64) -> ForkliftResult<Vec<u8>> {
+    let mut src_target: Vec<u8>;
+    if size <= readmax as i64 {
+        if size > 0 {
+            src_target = Vec::with_capacity(size as usize);
+            unsafe {
+                src_target.set_len(size as usize);
+            }
+        } else {
+            src_target = Vec::with_capacity(readmax as usize);
+            unsafe {
+                src_target.set_len(readmax as usize);
+            }
+        }
+    } else {
+        return Err(ForkliftError::FSError("File Name too long".to_string()));
+    }
+    Ok(src_target)
+}
+
 pub fn copy_link(
     src: &Entry,
     src_context: &NetworkContext,
@@ -258,22 +283,8 @@ pub fn copy_link(
     let src_size = src_stat.size();
     let src_path = src.path();
     let readmax = context.get_readmax()?;
-    let mut src_target: Vec<u8>;
-    if src_size <= readmax as i64 {
-        if src_size > 0 {
-            src_target = Vec::with_capacity(src_size as usize);
-            unsafe {
-                src_target.set_len(src_size as usize);
-            }
-        } else {
-            src_target = Vec::with_capacity(readmax as usize);
-            unsafe {
-                src_target.set_len(readmax as usize);
-            }
-        }
-    } else {
-        return Err(ForkliftError::FSError("File Name too long".to_string()));
-    }
+    let mut src_target: Vec<u8> = make_target(src_size, readmax)?;
+
     match context.readlink(src_path.as_path(), &mut src_target) {
         Ok(_) => (),
         Err(e) => {
@@ -296,22 +307,7 @@ pub fn copy_link(
             let dest_size = dest_stat.size();
             let dest_path = dest.path();
             let readmax = dcontext.get_readmax()?;
-            let mut dest_target: Vec<u8>;
-            if dest_size <= readmax as i64 {
-                if dest_size > 0 {
-                    dest_target = Vec::with_capacity(dest_size as usize);
-                    unsafe {
-                        dest_target.set_len(dest_size as usize);
-                    }
-                } else {
-                    dest_target = Vec::with_capacity(readmax as usize);
-                    unsafe {
-                        dest_target.set_len(readmax as usize);
-                    }
-                }
-            } else {
-                return Err(ForkliftError::FSError("File Name too long".to_string()));
-            }
+            let mut dest_target: Vec<u8> = make_target(dest_size, readmax)?;
             match dcontext.readlink(dest_path.as_path(), &mut dest_target) {
                 Ok(_) => (),
                 Err(e) => {
@@ -366,28 +362,54 @@ pub fn copy_link(
     Ok(outcome)
 }
 
+fn read_chunk(file: &FileType, offset: u64, path: &PathBuf) -> ForkliftResult<Vec<u8>> {
+    let buffer = match file.read(BUFF_SIZE, offset) {
+        Ok(buf) => buf,
+        Err(e) => {
+            let err = format!(
+                "Error {:?}, Could not read from {}",
+                e,
+                path.to_string_lossy(),
+            );
+            return Err(ForkliftError::FSError(err));
+        }
+    };
+    if buffer.len() <= 0 {
+        return Ok(buffer);
+    }
+    Ok(buffer)
+}
+
+fn open_file(
+    context: &mut NetworkContext,
+    path: &PathBuf,
+    flags: OFlag,
+    error: &str,
+) -> ForkliftResult<FileType> {
+    match context.open(path.as_path(), flags, Mode::S_IRWXU) {
+        Ok(f) => Ok(f),
+        Err(e) => {
+            let err = format!("Error {}, {}", e, error);
+            return Err(ForkliftError::FSError(err));
+        }
+    }
+}
+
 pub fn copy_entry(
     src: &Entry,
     src_context: &mut NetworkContext,
     dest: &Entry,
     dest_context: &mut NetworkContext,
 ) -> ForkliftResult<SyncOutcome> {
-    let src_file = match src_context.open(&src.path().as_path(), OFlag::O_RDONLY, Mode::S_IRWXU) {
-        Ok(f) => f,
-        Err(e) => {
-            println!("Error {:?}", e);
-            let err = format!(
-                "Could not open {} for reading",
-                src.path().to_string_lossy()
-            );
-            return Err(ForkliftError::FSError(err));
-        }
-    };
-
-    let src_meta = match src.metadata() {
-        Some(s) => s,
-        None => panic!("src meta should not be None"),
-    };
+    //check if src exists (which it should...)
+    if !exist(src.path(), src_context) {
+        panic!("Source file should exist!");
+    }
+    let err = format!(
+        "Could not open {} for reading",
+        src.path().to_string_lossy()
+    );
+    let src_file = open_file(src_context, src.path(), OFlag::O_RDONLY, &err)?;
 
     let dest_file = match dest_context.create(
         &dest.path().as_path(),
@@ -407,26 +429,11 @@ pub fn copy_entry(
 
     //let mut buffer = vec![0, BUFF_SIZE];
     let mut offset = 0;
-    let mut buffer;
     let mut end = false;
     while { !end } {
-        buffer = match src_file.read(BUFF_SIZE, offset) {
-            Ok(buf) => buf,
-            Err(e) => {
-                let err = format!(
-                    "Could not read from {}, {}",
-                    src.path().to_string_lossy(),
-                    e
-                );
-                return Err(ForkliftError::FSError(err));
-            }
-        };
-        if buffer.len() <= BUFF_SIZE as usize {
-            end = true;
-        }
-
-        match dest_file.write(&buffer, offset) {
-            Ok(_) => (),
+        let buffer = read_chunk(&src_file, offset, src.path())?;
+        let num_written = match dest_file.write(&buffer, offset) {
+            Ok(n) => n,
             Err(e) => {
                 let err = format!(
                     "Error {}, Could not write to {}",
@@ -436,9 +443,92 @@ pub fn copy_entry(
                 return Err(ForkliftError::FSError(err));
             }
         };
-        offset = offset + BUFF_SIZE;
+        if num_written == 0 {
+            end = true;
+        }
+        offset = offset + num_written as u64;
         //INSERT PROGRESS MESSAGE HERE
         //SEND PROGRESS
     }
     Ok(SyncOutcome::FileCopied)
+}
+
+pub fn checksum_copy(
+    src: &Entry,
+    src_context: &mut NetworkContext,
+    dest: &Entry,
+    dest_context: &mut NetworkContext,
+) -> ForkliftResult<SyncOutcome> {
+    //check if src exists (which it should...)
+    if !exist(src.path(), src_context) {
+        panic!("Source file should exist!");
+    }
+    // open src file
+    let err = format!(
+        "Could not open {} for reading",
+        src.path().to_string_lossy()
+    );
+    let src_file = open_file(src_context, src.path(), OFlag::O_RDONLY, &err)?;
+    //open dest file
+    let dest_file = open_file(
+        dest_context,
+        dest.path(),
+        OFlag::O_RDWR | OFlag::O_CREAT,
+        &err,
+    )?;
+
+    //loop until end
+    let mut offset = 0;
+    let mut meowhash = MeowHasher::new();
+    let mut end = false;
+    let mut counter = 0; //count number of times we needed to update the file
+    while { !end } {
+        let mut num_written: i32 = 0;
+        //read 1M from src
+        let src_buf = read_chunk(&src_file, offset, src.path())?;
+        //hash src_buf
+        meowhash.input(&src_buf);
+        let hash_src = meowhash.result_reset();
+        //read 1M from dest
+        let dest_buf = read_chunk(&dest_file, offset, dest.path())?;
+        //hash dest_buf
+        meowhash.input(&dest_buf);
+        let hash_dest = meowhash.result_reset();
+        //if hash_src != has_dest
+        if hash_src != hash_dest {
+            if src_buf.len() < dest_buf.len() {
+                dest_file.truncate(src_buf.len() as u64)?;
+            }
+
+            //write src_buf -> dest
+            num_written = match dest_file.write(&src_buf, offset) {
+                Ok(n) => n,
+                Err(e) => {
+                    let err = format!(
+                        "Error {}, Could not write to {}",
+                        e,
+                        dest.path().to_string_lossy()
+                    );
+                    return Err(ForkliftError::FSError(err));
+                }
+            };
+            //update counter
+            counter += 1;
+        }
+        //update offset
+        //check if num_written > 0
+        if num_written > 0 {
+            offset = 0;
+        } else {
+            offset += src_buf.len() as u64;
+        }
+        if src_buf.len() == 0 {
+            end = true;
+        }
+    } //end loop
+
+    if counter == 0 {
+        return Ok(SyncOutcome::UpToDate);
+    }
+    Ok(SyncOutcome::FileUpdated)
 }
